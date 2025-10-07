5f69c9386f7b376172425be1321850f01c185a56
edgaol <edgaol99@gmail.com>
2025-10-07 15:18:42 -0300

    Fixed partial pressure exercise examples

--- quichem/examples/gases_examples.py
+++ quichem/examples/gases_examples.py
@@ -7,13 +7,13 @@ parent_dir = os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))
 sys.path.insert(0, parent_dir)
 
 from core import Molecule
-from gases import ideal_gases, u
+from gases import law, u
 
 def ex_4_1():
     v0 = 2.00 * u.L
     p0 = 2.00 * u.atm
     p = 3.00 * u.atm
-    v = ideal_gases(p0=p0, v0=v0, p=p)
+    v = law(p0=p0, v0=v0, p=p)
     print(f"4.1) The new volume is {v}.")
 
 def ex_4_2():
@@ -26,7 +26,7 @@ def ex_4_3():
     v0 = 2 * u.L
     t0 = 273.15 * u.K
     t = 373.15 * u.K
-    v = ideal_gases(v0=v0, t0=t0, t=t)
+    v = law(v0=v0, t0=t0, t=t)
     print(f"4.3) The new volume is {v}.")
 
 def ex_4_4():
@@ -35,21 +35,21 @@ def ex_4_4():
     t0 = 283.15 * u.K
     t = 373.15 * u.K
     p = 1 * u.atm
-    v = ideal_gases(v0=v0, p0=p0, t0=t0, t=t, p=p)
+    v = law(v0=v0, p0=p0, t0=t0, t=t, p=p)
     print(f"4.4) The new volume is {v}.")
 
 def ex_4_5():
     n = 2
     p = 1.5
     t = 400.15
-    v = ideal_gases(n=n, p=p, t=t)
+    v = law(n=n, p=p, t=t)
     print(f"4.5) The new volume is {v}.")
 
 def ex_4_6():
     co2 = Molecule("CO2", 22 * u.g)
     p = 0.9
     t = 283.15
-    v = ideal_gases(p=p, t=t, n=co2.mol)
+    v = law(p=p, t=t, n=co2.mol)
     print(f"4.6) The volume is {v}.")
 
 def ex_4_7():
@@ -57,31 +57,37 @@ def ex_4_7():
     p0 = 1
     t0 = 283.15
     t = 373.15
-    p = ideal_gases(p0=p0, t0=t0, t=t)
+    p = law(p0=p0, t0=t0, t=t)
     print(f"4.7) The new pressure is {p}.")
 
-# Partial pressure section. To correct
 def ex_4_8():
+    # Oxygen gas volume approximately 21% of air's
     t0 = 298.15
     p0 = 0.93
-    v = ideal_gases(t0=t0, p0=p0)
-    print(f"4.8) The volume will be of {v}.")
+    v = law(t0=t0, p0=p0)
+    vo2 = 0.21 * v
+    print(f"4.8) The volume will be of {vo2}.")
 
 def ex_4_9():
-    n0 = 0.4
+    # Reaction: 4 TNG(l) -> 6 N2(g) + 12 CO2(g) + 10 H2O(g) + 1 O2(g)
+    # Since the TNG's volume is negligible, the initial mol count (n) for the air occupying the recipient needs to be calculated.
+    # Given stoichometry, there will produced a total of 2.9 mol of gas by the 0.4 inital TNG. There is also 0.4 mol of air, so 3.3 mol total gas.
     p0 = 1
+    v0 = 10
     t0 = 298.15
     t = 573.15
-    n = n0 * (1 + 3)
-    p = ideal_gases(n=n, t=t, p0=p0, t0=t0)
+    # _n0 = law(p=1, v=v0, t=t0)
+    n = 3.3
+    p = law(n=n, t=t, v=v0)
     print(f"4.9) The new pressure would be of {p}.")
 
 def ex_4_10():
-    kco3 = Molecule("KCO3", 3 * u.g)
-    o2 = Molecule("O2", kco3.grams * (3*pt.O.mass*u.g/u.mole / kco3.mw))
+    # Reaction: KClO3 -> KCl + 3/2 O2
+    kclo3 = Molecule("KClO3", 3 * u.g)
+    o2 = Molecule("O2", kclo3.mol * 3/2)
     p = 0.93
     t = 298.15
-    v = ideal_gases(n=o2.mol, t=t, p=p)
+    v = law(n=o2.mol, t=t, p=p)
     print(f"4.10) The volume is {v}.")
 
 if __name__ == "__main__":
